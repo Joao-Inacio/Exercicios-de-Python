42ae4ca3dc23609f32fd9bd441f0f381a894a11a
João Inácio <joaoinacio206@protonmail.com>
2021-06-13 17:46:16 -0300

    Create Exe002.py

---
+++ Exe002.py
@@ -0,0 +1,8 @@
+"""2- crie um script python que leia o dia, o mês  e o ano de nascimento de
+uma pessoa e mostre uma mensagem com a data formatada
+"""
+dia = input('Dia do nascimento: ')
+mes = input('Mês de nascimento: ')
+ano = input('Ano de nascimento: ')
+print('Você nasceu no dia', dia, 'Mês de', mes, 'no ano de', ano)
+
