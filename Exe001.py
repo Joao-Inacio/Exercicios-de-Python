1b7118389f266eeb715085001c93983a1978ecfc
João Inácio <joaoinacio206@protonmail.com>
2021-06-13 10:43:21 -0300

    Create Exe001.py

---
+++ Exe001.py
@@ -0,0 +1,5 @@
+"""
+1- Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
+"""
+nome = input('Qual é seu nome: ')
+print('Olá', nome, ' Seja Bem Vindo(a)')
