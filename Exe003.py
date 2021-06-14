c347efa8b2f1612b503f3e144c7875be447b31c0
João Inácio <joaoinacio206@protonmail.com>
2021-06-14 09:12:49 -0300

    Create Exe003.py

---
+++ Exe003.py
@@ -0,0 +1,7 @@
+"""
+3- Crei um script Python que leia dois números e 'tente' mostrar a soma entre eles
+"""
+n1 = input('Digite um número: ')
+n2 = input('Digite outro número: ')
+print('O resultado é', n1 + n2)
+
