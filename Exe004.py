aea6a65add2a8eddb0607ecc3d74597e4a979308
João Inácio <joaoinacio206@protonmail.com>
2021-06-21 19:03:36 -0300

    Update Exe004.py

--- Exe004.py
+++ Exe004.py
@@ -1,4 +1,10 @@
 p = input('Escreva algo:')
 print(f'é um digito {p.isdigit()}\n'
+      f'o tipo é {type(p)}\n'
+      f'só tem espaços {p.isspace()}\n'
       f'é um número {p.isnumeric()}\n'
-      f'é uma palavra {p.isalnum()}')
+      f'é alfabético? {p.isalpha()}\n'
+      f'Esta em maiúscula? {p.isupper()}\n'
+      f'Esta em minúsculas? {p.islower()}\n'
+      f'Está capitalizada? {p.istitle()}\n'
+      f'é uma alfanumérico {p.isalnum()}')
