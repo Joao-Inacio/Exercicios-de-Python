b70703321a7b410698f1ec7bbea007f62e2d251d
João Inácio <joaoinacio206@protonmail.com>
2021-06-23 18:29:39 -0300

    Create Exe005.py

---
+++ Exe005.py
@@ -0,0 +1,5 @@
+"""
+Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+"""
+n = int(input('Digite um número: '))
+print(f'{n - 1} é o antecessor de {n} e {n + 1} é o sucessor')
