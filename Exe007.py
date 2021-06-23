9a5e1854feb51d3c97ed069b751290ce95efda10
João Inácio <joaoinacio206@protonmail.com>
2021-06-23 18:40:00 -0300

    Create Exe007.py

---
+++ Exe007.py
@@ -0,0 +1,9 @@
+"""
+Desenvolva um programa que leia as duas notas de um aluno calcule e mostre a sua média
+"""
+print('==' * 25)
+n1 = float(input('Primeira Nota: '))
+n2 = float(input('Segunda Nota: '))
+total = (n1 + n2) / 2
+print(f'Sua média é {total}')
+
