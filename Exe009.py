d56007bf51b74181f4e62aac9a03f4964b0f4841
João Inácio <joaoinacio206@protonmail.com>
2021-06-28 09:04:13 -0300

    Create Exe009.py

---
+++ Exe009.py
@@ -0,0 +1,15 @@
+"""
+ Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+"""
+n = int(input('Tabuada do número: '))
+print(f'{n} + 0 = {n+0}\n'
+      f'{n} + 1 = {n+1}\n'
+      f'{n} + 2 = {n+2}\n'
+      f'{n} + 3 = {n+3}\n'
+      f'{n} + 4 = {n+4}\n'
+      f'{n} + 5 = {n+5}\n'
+      f'{n} + 6 = {n+6}\n'
+      f'{n} + 7 = {n+7}\n'
+      f'{n} + 8 = {n+8}\n'
+      f'{n} + 9 = {n+9}\n'
+      f'{n} + 10 = {n+10}')
