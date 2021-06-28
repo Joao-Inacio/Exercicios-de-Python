a1540784df660d117453c53d2ee55c196d6e464f
João Inácio <joaoinacio206@protonmail.com>
2021-06-28 09:08:22 -0300

    Create Exe010.py

---
+++ Exe010.py
@@ -0,0 +1,7 @@
+"""
+Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e
+ mostre quantos dólares ela pode comprar  1,00 = 3,27
+"""
+dinhero = float(input('Quantia de Dinheiro: '))
+print(f'Você tem R${dinhero} e US${dinhero * 3.27}')
+
