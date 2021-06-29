9eb06618f08d22e918255eec5b396a117e423049
João Inácio <joaoinacio206@protonmail.com>
2021-06-29 18:23:30 -0300

    Update Exe006.py

--- Exe006.py
+++ Exe006.py
@@ -2,4 +2,4 @@
 Crie um algoritmo que leia um número e mostre o seu dobro triplo e a raiz quadrada
 """
 n = int(input('Digite um número: '))
-print(f'O dobro de {n} é {n + n} e seu triplo é {n * 3} e sua raiz quadrada é {n ** 0.5}')
+print(f'O dobro de {n} é {n + n}\n e seu triplo é {n * 3}\n e sua raiz quadrada é {n ** 0.5:.2f}')
