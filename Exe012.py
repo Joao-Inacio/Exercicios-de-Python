6b39543acbd4544bc01ecca205bcb5ef4dc5c164
João Inácio <joaoinacio206@protonmail.com>
2021-07-04 16:33:12 -0300

    Update Exe012.py

--- Exe012.py
+++ Exe012.py
@@ -1,6 +1,6 @@
 """
 Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto
 """
-p = float(input('Preço: R$ '))
+p = float(input('Preço: R$'))
 s = (p * 5) / 100
 print(f'O novo preço é R${p - s}')
