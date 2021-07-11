621c4b2c935fafec79862c41ae7496b1b8c889da
João Inácio <joaoinacio206@protonmail.com>
2021-07-11 18:54:10 -0300

    Tocando Musicas

---
+++ Exe021.py
@@ -0,0 +1,7 @@
+"""
+ Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+"""
+from audioplayer import AudioPlayer
+
+AudioPlayer("C:\ExercícioPython\Exercicios-de-Python\music.mp3").play(block=True)
+# Link do Módulo https://pypi.org/project/audioplayer/
