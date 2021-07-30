28bc12916911fa67f5aa611d45703e7aacef1c05
João Inácio <joaoinacio206@protonmail.com>
2021-07-30 09:08:33 -0300

    Create Exe030.py

---
+++ Exe030.py
@@ -0,0 +1,5 @@
+"""
+     Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+"""
+n = int(input('Digite um número: '))
+print(f'O Número {n} é Par' if n % 2 == 0 else f'O Número {n} é Ímpar')
